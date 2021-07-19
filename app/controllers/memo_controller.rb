a104c9410230250d16dd5a870209b1b2105ebcb3
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-07-19 23:26:52 +0900

    リダリクト処理

--- app/controllers/memo_controller.rb
+++ app/controllers/memo_controller.rb
@@ -1,4 +1,13 @@
 class MemoController < ApplicationController
+  before_action :move_to_index, except: [:index]
   def index
+  end
+end
+
+private
+
+def move_to_index
+  unless user_signed_in?
+    redirect_to action: :index
   end
 end
